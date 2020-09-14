6d31d545de9ea9a17964ed3c5c32b47f579e5992
matheus.ferreira <matheus.ferreira@cabal.com.br>
2020-09-14 10:58:02 -0300

    Application configuration.

---
+++ src/main/java/study/projects/exemgenerator/ExemGeneratorApplication.java
@@ -0,0 +1,13 @@
+package study.projects.exemgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExemGeneratorApplication.class, args);
+    }
+
+}
